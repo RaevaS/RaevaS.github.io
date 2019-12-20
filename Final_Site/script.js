2bc8f355a70b7e4dce5472086cf6d5c07a691950
Raeva Sayed <ras980@nyu.edu>
2019-12-19 22:17:44 -0500

    Adding Final Site

--- Final_Site/script.js
+++ Final_Site/script.js
@@ -10,33 +10,46 @@ var lineChartData = {
     labels: ["Dec 1", "Dec 2", "Dec 3", "Dec 4", "Dec 5", "Dec 6", "Dec 7"],
     datasets: [{
         //happy
-        fillColor: "rgba(220,220,220,0)",
-        strokeColor: "rgba(220,180,0,1)",
-        pointColor: "rgba(220,180,0,1)",
+        label: "Happy",
+        fillColor: "rgba(255,230,57,0)",
+        strokeColor: "rgba(255,230,57,1)",
+        pointColor: "rgba(255,230,57,1)",
         data: [4, 2, 6, 3, 8, 8, 7]
     }, {
         //sad
-        fillColor: "rgba(151,187,205,0)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
+        label: "Sad",
+        fillColor: "rgba(14,100,232,0)",
+        strokeColor: "rgba(14,100,232,1)",
+        pointColor: "rgba(14,100,232,1)",
         data: [5, 3, 0, 7, 2, 0, 5]
     },
     {
         //angry
-        fillColor: "rgba(255,227,0,0)",
-        strokeColor: "rgba(255,227,0,1)",
-        pointColor: "rgba(255,227,0,1)",
+        label: "Angry",
+        fillColor: "rgba(191,13,0,0)",
+        strokeColor: "rgba(191,13,0,1)",
+        pointColor: "rgba(191,13,0,1)",
         data: [4, 7, 1, 7, 4, 5, 0]
     }, {
 
         //hungry/thirsty
-        fillColor: "rgba(255,0,255,0)",
-        strokeColor: "rgba(255,0,255,1)",
-        pointColor: "rgba(255,0,255,1)",
+        label: "Hungry/Thirsty",
+        fillColor: "rgba(230,170,154,0)",
+        strokeColor: "rgba(230,170,154,1)",
+        pointColor: "rgba(230,170,154,1)",
         data: [0, 8, 2, 4, 3, 7, 1]
-    }]
+    }],
+    options:{
+        legend: {
+            display: 'true',
+            labels:{
+                fontColor: 'rgb(255, 255, 255)'
+            },
+            position: 'bottom',
+        },
+    },
 
-}
+};
 
 Chart.defaults.global.animationSteps = 80;
 Chart.defaults.global.tooltipYPadding = 16;
@@ -47,6 +60,7 @@ Chart.defaults.global.animationEasing = "easeInSine";
 Chart.defaults.global.responsive = true;
 Chart.defaults.global.scaleLineColor = "black";
 Chart.defaults.global.scaleFontSize = 16;
+Chart.defaults.global.legend = true;
 
 var ctx = document.getElementById("canvas").getContext("2d");
 var LineChartDemo = new Chart(ctx).Line(lineChartData, {
@@ -54,4 +68,5 @@ var LineChartDemo = new Chart(ctx).Line(lineChartData, {
     bezierCurve: true,
     scaleShowVerticalLines: false,
     scaleGridLineColor: "black"
-});
+}
+);
